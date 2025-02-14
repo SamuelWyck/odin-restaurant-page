0a2da73af88dbd2dfd0e1286171a3912d2646346
samuel <samawyck@gmail.com>
2025-02-14 11:03:39 -0600

    Handle images in js

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: "asset/resource",
+            },
         ],
     },
 };
