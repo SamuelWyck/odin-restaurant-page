aacb0a214fadbc27ad991b2b11b9ab451a4e382a
samuel <samawyck@gmail.com>
2025-02-17 12:22:30 -0600

    Create page handling logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import "./styles.css";
 import homePage from "./home-page";
 
-homePage.createPage();
+
+const pageHandler = (function() {
+    const header = document.querySelector("header");
+    const contentDiv = document.querySelector("#content");
+    const home = homePage;
+
+    showHomePage();
+    
+
+    header.addEventListener("click", function(event) {
+        if (event.target.matches(".banner")) {
+            showHomePage();
+        } else if (event.target.matches(".menu-btn")) {
+
+        } else if (event.target.matches(".order-btn")) {
+
+        } else if (event.target.matches("about-btn")) {
+
+        }
+    });
+
+    function showHomePage() {
+        clearContent();
+        home.createPage();
+    };
+
+    function clearContent() {
+        contentDiv.innerHTML = "";
+    };
+
+})();
