b0bf4b0933e96c14232ace2de80f2b698f49684f
samuel <samawyck@gmail.com>
2025-02-17 15:37:16 -0600

    Create currentDayText content

--- src/about.js
+++ src/about.js
@@ -5,7 +5,7 @@ import currentPic from "./imgs/current.jpeg";
 const about = (function() {
     const contentDiv = document.querySelector("#content");
     const founderText = "In 1647, in a little village outside of Paris, our founder, Monsieur Poulet le Coq, made a groundbreaking discovery. While experimenting with new chicken recipes he realized that using rubber chickens would make sure his food was always al dente. With this discovery Le Coq immediately opened a restaurant to sell food made with rubber chickens. The restaurant was a huge success and Le Coq was the proud owner for 67.5 years before his death in 1714. His final wish was that his family continue running the restaurant so that the art of rubber chicken cooking would not be lost to the world.";
-    const currentDayText = "Today ";
+    const currentDayText = "Today Le Caoutchouc Poulet still honors Monsieur Poulet le Coq's wishes. We proudly trace our ownership through an unbroken line of Coqs all the way back to Monsieur Poulet le Coq himself. Through the years, though we have grown and opened new locations across the world, we still hold to our original mission to provide the best quality rubber chicken in the world";
 
 
     function createImg(img) {
