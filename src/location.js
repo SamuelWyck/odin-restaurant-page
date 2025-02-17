215bacec49d467358b4eb32aa0ce9bf97ae8d222
samuel <samawyck@gmail.com>
2025-02-17 17:31:39 -0600

    Create address variables

--- src/location.js
+++ src/location.js
@@ -3,7 +3,19 @@ import FrenchPic from "./imgs/french.jpeg";
 
 
 const location = (function() {
+    const contentDiv = document.querySelector("div");
+    const usAddress1 = "145 West 53rd Street, New York, New York 10019";
+    const usAddress2 = "3655 South Las Vegas Blvd South, Las Vegas, Nevada 89109";
+    const frenchAddress1 = "52 Rue Francois Miron, 75004 Paris, France";
+    const frenchAddress2 = "Parc du Chateau de, 78000 Versailles, France";
 
+    let createPage = function() {
+        const locationDiv = document.createElement("div");
+        locationDiv.classList.add("location-content");
+
+    };
+
+    return {"createPage": createPage};
 })();
 
 export default location;
